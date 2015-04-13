39d5a6feb9056f6c69791da2013bf127d0c6ee05
Sebastian Winterling <sebastian.winterling@iem.thm.de>
2015-04-13 17:53:17 +0200

    1.0.19

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,8 @@
         db = window.openDatabase("PhyLabDB", "", "PhyLab Database", 1024*1024);
         createDBTables();
         fillDBTables();
-        $(':mobile-pagecontainer').pagecontainer('change', '#startPage', {});                       
+        $(':mobile-pagecontainer').pagecontainer('change', '#startPage', {});
+        navigator.splashscreen.hide();                       
     });
     
          
