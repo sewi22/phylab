f2b2ec13b7404ea0d8f9c7401c8a6b70c9627122
Sebastian Winterling <sebastian.winterling@iem.thm.de>
2015-06-22 13:41:33 +0200

    1.1.56

--- js/video.js
+++ js/video.js
@@ -7,14 +7,15 @@
     
     $(document).on('pagebeforeshow', '#videoPage', function(e) {
         $("#videoContent").empty();        
-        //$("#videoContent").append('<iframe id="player" type="text/html" width="320" height="250" src="http://www.youtube.com/embed/u1zgFlCw8Aw?enablejsapi=1&origin=http://example.com" frameborder="0" allowfullscreen></iframe>');        
-        $("#videoContent").append('<iframe id="player" type="text/html" src="https://www.youtube.com/watch?v=OTglg05fyHg?enablejsapi=1" frameborder="0" allowfullscreen></iframe>');        
+        //$("#videoContent").append('<iframe id="player" type="text/html" width="320" height="250" src="http://www.youtube.com/embed/u1zgFlCw8Aw?enablejsapi=1&origin=http://example.com" frameborder="0" allowfullscreen></iframe>');
+                
+        $("#videoContent").append('<iframe id="player" type="text/html" src="http://www.youtube.com/embed/OTglg05fyHg?enablejsapi=1" frameborder="0" allowfullscreen></iframe>');        
         $("#videoContent").enhanceWithin();
         
         var expGroupNumber = localStorage.getItem("expGroupNumber");
         var expNumber = localStorage.getItem("expNumber");
         getExp(expGroupNumber, expNumber, function(result){
-            alert(result);
+            alert(result.videolink);
             //$("#videoContent").append('<iframe id="player" type="text/html" src="'+result.videolink+'?enablejsapi=1" frameborder="0" allowfullscreen></iframe>');
             //$("#videoContent").enhanceWithin();
         });
